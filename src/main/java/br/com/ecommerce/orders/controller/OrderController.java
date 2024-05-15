ba44d687385e141267ee60f3bed16a182da5cce0
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 10:12:10 -0300

    Add `create` method to `OrderController`

--- src/main/java/br/com/ecommerce/orders/controller/OrderController.java
+++ src/main/java/br/com/ecommerce/orders/controller/OrderController.java
@@ -1,10 +1,23 @@
 package br.com.ecommerce.orders.controller;
 
+import java.util.List;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ecommerce.orders.model.PaymentDTO;
+import br.com.ecommerce.orders.model.order.Order;
+import br.com.ecommerce.orders.model.order.OrderCreateDTO;
+import br.com.ecommerce.orders.model.product.StockWriteOffDTO;
 import br.com.ecommerce.orders.service.OrderService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/orders")
@@ -12,4 +25,27 @@ public class OrderController {
 
 	@Autowired
 	private OrderService service;
+	@Autowired
+	private RabbitTemplate template;
+	
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<?> create(
+			@RequestBody @Valid OrderCreateDTO dto, 
+			@RequestHeader("X-auth-user-id") Long userId
+			) {
+		Order order = service.saveOrder(dto, userId);
+		
+		PaymentDTO paymentCreate = new PaymentDTO(
+				order.getId(), order.getUserId(), order.getTotal());
+		
+		List<StockWriteOffDTO> stockUpdate = order.getProducts().stream()
+				.map(o -> new StockWriteOffDTO(o.getProductId(), o.getUnit()))
+				.toList();
+		
+		template.convertAndSend("order.create.ex", "payment", paymentCreate);
+		template.convertAndSend("order.create.ex", "stock", stockUpdate);
+		return ResponseEntity.ok().build();
+	}
 }
