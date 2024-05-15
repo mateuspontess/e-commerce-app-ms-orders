486d75e962c6c3ce1fbd976fd6e2ac21eb75bc09
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 10:05:43 -0300

    Add `AllowedStatusTransitions` class

---
+++ src/main/java/br/com/ecommerce/orders/model/order/AllowedStatusTransitions.java
@@ -0,0 +1,22 @@
+package br.com.ecommerce.orders.model.order;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class AllowedStatusTransitions {
+
+	private static final Map<OrderStatus, List<OrderStatus>> map = new HashMap<OrderStatus, List<OrderStatus>>();
+	
+	static {
+		map.put(OrderStatus.AWAITING_PAYMENT, Arrays.asList(OrderStatus.CONFIRMED_PAYMENT, OrderStatus.CANCELED));
+		map.put(OrderStatus.CONFIRMED_PAYMENT, Arrays.asList(OrderStatus.IN_TRANSIT, OrderStatus.CANCELED));
+		map.put(OrderStatus.IN_TRANSIT, Arrays.asList(OrderStatus.DELIVERED));
+	}
+	
+	public static List<OrderStatus> getAllowedOrderStatus(OrderStatus status) {
+		return map.getOrDefault(status, null);
+	}
+}
