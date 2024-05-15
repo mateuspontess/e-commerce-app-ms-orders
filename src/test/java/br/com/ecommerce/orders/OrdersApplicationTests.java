92daf8f74fe9b179b502ac53330ce15f2538c9d2
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 09:13:20 -0300

    Add project

---
+++ src/test/java/br/com/ecommerce/orders/OrdersApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.ecommerce.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
