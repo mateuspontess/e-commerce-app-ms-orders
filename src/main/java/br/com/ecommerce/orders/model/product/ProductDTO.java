e878abeb7d825e7030ffc72b2a4d6ded27979a79
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 09:27:19 -0300

    Add `ProductDTO` class

---
+++ src/main/java/br/com/ecommerce/orders/model/product/ProductDTO.java
@@ -0,0 +1,16 @@
+package br.com.ecommerce.orders.model.product;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ProductDTO(
+		
+		@NotNull
+		Long id,
+		@NotNull
+		Integer unit
+		) {
+	
+	public ProductDTO(Product p) {
+		this(p.getProductId(), p.getUnit());
+	}
+}
