27a508fcb4f3346b8bc8cb59feb0324218d13bbc
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 09:58:49 -0300

    Add `OrderBasicInfoDTO` class

---
+++ src/main/java/br/com/ecommerce/orders/model/order/OrderBasicInfDTO.java
@@ -0,0 +1,28 @@
+package br.com.ecommerce.orders.model.order;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public record OrderBasicInfDTO(
+		
+		Long id,
+		
+		BigDecimal totalOrder,
+		
+		@JsonFormat(pattern = "dd/MM/yyyy")
+		LocalDate date,
+		
+		OrderStatus status
+		) {
+	
+	public OrderBasicInfDTO(Order o) {
+		this(
+				o.getId(),
+				o.getTotal(),
+				o.getDate(),
+				o.getStatus()
+			);
+	}
+}
