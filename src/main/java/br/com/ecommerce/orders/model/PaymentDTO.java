7d4a7774b9fe3413895a5f1bbba1da1d74dfed03
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 10:11:49 -0300

    Add `PaymentDTO` and `StockWriteOffDTO` classes

---
+++ src/main/java/br/com/ecommerce/orders/model/PaymentDTO.java
@@ -0,0 +1,11 @@
+package br.com.ecommerce.orders.model;
+
+import java.math.BigDecimal;
+
+public record PaymentDTO(
+		
+		Long orderId,
+		Long userId,
+		BigDecimal paymentAmount
+		) {
+}
---
+++ src/main/java/br/com/ecommerce/orders/model/product/StockWriteOffDTO.java
@@ -0,0 +1,7 @@
+package br.com.ecommerce.orders.model.product;
+
+public record StockWriteOffDTO(
+		
+		Long productId, 
+		Integer unit
+		) {}
