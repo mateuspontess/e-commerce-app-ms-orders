791eb6990355cde919124d0d7b10afd67a64d4db
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-24 10:51:50 -0300

    Remove `OrderValidationException` class

--- src/main/java/br/com/ecommerce/orders/exception/OrderValidationException.java
+++
@@ -1,9 +0,0 @@
-package br.com.ecommerce.orders.exception;
-
-public class OrderValidationException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public OrderValidationException(String message) {
-		super(message);
-	}
-}
