769d57a8ad3d2e9069fa4d396a4b3b2b1064f78f
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-06 17:27:33 -0300

    refactor: `Order` class
    - add isValidStatusTransition method
    - add checkNotNull method
    - refactor updateOrderStatus method
    - refactor args constructor
    - add @Builder and @AllArgsConstructor annotations

--- src/main/java/br/com/ecommerce/orders/model/order/Order.java
+++ src/main/java/br/com/ecommerce/orders/model/order/Order.java
@@ -15,9 +15,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Entity(name = "Order")
@@ -41,27 +45,59 @@ public class Order {
 	
 	
 	public Order(Long userId, List<Product> products) {
+		this.checkNotNull(userId, "userId");
+
+		if (products == null || products.isEmpty())
+			throw new IllegalArgumentException("Cannot me null or empty: products");
+
 		this.products = products;
 		this.userId = userId;
+
 		this.status = OrderStatus.AWAITING_PAYMENT;
 		this.total = this.calculateTotalOrderValue();
 		this.date = LocalDate.now();
 	}
 
+	public void updateOrderStatus(OrderStatus newStatus) {
+		if(!this.isValidStatusTransition(newStatus))
+			throw new IllegalArgumentException("The status " + this.status + " cannot transition to " + newStatus);
+		this.status = newStatus;
+	}
+	
 	private BigDecimal calculateTotalOrderValue() {
 		return this.products.stream()
 				.map(p -> p.getPrice().multiply(new BigDecimal(p.getUnit())))
 				.reduce(BigDecimal.ZERO, BigDecimal::add);
 	}
 	
-	public void updateOrderStatus(OrderStatus newStatus) {
-		List<OrderStatus> allowedStatuses = AllowedStatusTransitions.getAllowedOrderStatus(this.status);
-		if(allowedStatuses == null || !allowedStatuses.contains(newStatus)) 
-			throw new IllegalArgumentException("The status " + this.status + " cannot transition to " + newStatus);
-		
-		this.status = newStatus;
+	private void checkNotNull(Object field, String fieldName) {
+		if (field == null)
+			throw new IllegalArgumentException("Cannot be null: " + fieldName); 
 	}
-	
+
+	private boolean isValidStatusTransition(OrderStatus newStatus) {
+		if (this.status == OrderStatus.AWAITING_PAYMENT) {
+			return List.of(OrderStatus.CONFIRMED_PAYMENT, OrderStatus.CANCELED)
+				.contains(newStatus);
+		}
+		if (this.status == OrderStatus.CONFIRMED_PAYMENT) {
+			return List.of(OrderStatus.IN_TRANSIT, OrderStatus.CANCELED)
+				.contains(newStatus);
+		}
+		if (this.status == OrderStatus.IN_TRANSIT) {
+			return List.of(OrderStatus.DELIVERED)
+				.contains(newStatus);
+		}
+		
+		if (this.status == OrderStatus.DELIVERED) {
+			return false;
+		}
+		if (this.status == OrderStatus.CANCELED) {
+			return false;
+		}
+		return false;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("Order[userId=%d, total=%s, status=%s]", this.userId, this.total.toString(), this.status);
