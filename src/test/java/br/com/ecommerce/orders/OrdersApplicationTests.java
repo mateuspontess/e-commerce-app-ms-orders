bdb0f68c4d389f7d2ef83a785e80bce7f5ae3f44
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-07 15:35:36 -0300

    test: implement context test

--- src/test/java/br/com/ecommerce/orders/OrdersApplicationTests.java
+++ src/test/java/br/com/ecommerce/orders/OrdersApplicationTests.java
@@ -1,9 +1,17 @@
 package br.com.ecommerce.orders;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.ecommerce.orders.testcontainers.EurekaTestContainer;
+import br.com.ecommerce.orders.testcontainers.MySQLTestContainer;
+import br.com.ecommerce.orders.testcontainers.RabbitMQTestContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith({EurekaTestContainer.class, RabbitMQTestContainer.class, MySQLTestContainer.class})
 class OrdersApplicationTests {
 
 	@Test
