529b2ad6baf03721ffa07a6c2863963cca198d83
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-17 17:35:16 -0300

    test: add `OrderStatusUnitTest`

---
+++ src/test/java/br/com/ecommerce/orders/unit/OrderStatusUnitTest.java
@@ -0,0 +1,27 @@
+package br.com.ecommerce.orders.unit;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.ecommerce.orders.model.order.OrderStatus;
+
+
+class OrderStatusUnitTest {
+
+	@Test
+	void fromStringTest01() {
+		assertThrows(IllegalArgumentException.class, () -> OrderStatus.fromString("non-existent"));
+	}
+
+	@Test
+	void fromStringTest02() {
+		var values = Arrays.asList(OrderStatus.values());
+		values.forEach(value -> {
+			assertDoesNotThrow(() -> OrderStatus.fromString(value.toString()));
+		});
+	}
+}
