ad9b71514f2ccb6ed34104888d7d665b7162d8ae
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-19 16:05:08 -0300

    feat: add `handleError400MissingRequestHeaderException` to `GlobalExceptionHandler`

--- src/main/java/br/com/ecommerce/orders/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/ecommerce/orders/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -34,13 +35,13 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(OutOfStockException.class)
 	public ResponseEntity<ErrorMessageWithProducts> handlerError400(OutOfStockException ex) {
 		return ResponseEntity
-				.badRequest()
-				.body(new ErrorMessageWithProducts(
-						HttpStatus.BAD_REQUEST.value(), 
-						ex.getMessage(),
-						ex.getProducts()
-						)
-					);
+			.badRequest()
+			.body(new ErrorMessageWithProducts(
+				HttpStatus.BAD_REQUEST.value(), 
+				ex.getMessage(),
+				ex.getProducts()
+				)
+			);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -48,13 +49,18 @@ public class GlobalExceptionHandler {
     	var fields = ex.getFieldErrors().stream().collect(Collectors.toMap(f -> f.getField().toString(), f -> f.getDefaultMessage()));
     	
     	return ResponseEntity
-    			.badRequest()
-    			.body(new ErrorMessageWithFields(
-					HttpStatus.BAD_REQUEST.value(),
-					METHOD_ARGUMENT_NOT_VALID_MESSAGE,
-					fields
-					)
-				);
+			.badRequest()
+			.body(new ErrorMessageWithFields(
+				HttpStatus.BAD_REQUEST.value(),
+				METHOD_ARGUMENT_NOT_VALID_MESSAGE,
+				fields
+				)
+			);
+    }
+
+	@ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorMessage> handleError400MissingRequestHeaderException(MissingRequestHeaderException ex) {
+        return ResponseEntity.badRequest().body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), "Required request header is missing"));
     }
 
 	@ExceptionHandler(IllegalArgumentException.class)
@@ -70,9 +76,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<ErrorMessage> handlerErro415(HttpMediaTypeNotSupportedException ex) {
         String unsupported = ex.getContentType() != null ? ex.getContentType().getType() + "/" + ex.getContentType().getSubtype() : "unknown";
+
         String supported = ex.getSupportedMediaTypes().stream()
-                              .map(mediaType -> mediaType.getType() + "/" + mediaType.getSubtype())
-                              .collect(Collectors.joining(", "));
+			.map(mediaType -> mediaType.getType() + "/" + mediaType.getSubtype())
+			.collect(Collectors.joining(", "));
         String message = String.format("Unsupported media type '%s'. Supported media types are: %s", unsupported, supported);
 
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).body(new ErrorMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), message));
@@ -81,12 +88,12 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleError500(Exception ex) {
         return ResponseEntity
-        		.internalServerError()
-        		.body(new ErrorMessage(
-        				HttpStatus.INTERNAL_SERVER_ERROR.value(),
-        				INTERNAL_SERVER_ERROR_MESSAGE
-						)
-					);
+			.internalServerError()
+			.body(new ErrorMessage(
+				HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				INTERNAL_SERVER_ERROR_MESSAGE
+				)
+			);
     }
 	
 	private record ErrorMessage(int status, String error) {}
