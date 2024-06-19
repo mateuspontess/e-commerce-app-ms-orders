7e1867ae5e3a0d76bd2ceaf5e824e0ee93378587
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-19 16:10:18 -0300

    test: refactor: `ÒrderControllerUnitTest`
    - identation correction
    - new tests added
    - new scenarios added
    - test improvements

--- src/test/java/br/com/ecommerce/orders/unit/OrderControllerUnitTest.java
+++ src/test/java/br/com/ecommerce/orders/unit/OrderControllerUnitTest.java
@@ -2,7 +2,13 @@ package br.com.ecommerce.orders.unit;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,6 +29,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import br.com.ecommerce.orders.model.PaymentDTO;
 import br.com.ecommerce.orders.model.order.Order;
 import br.com.ecommerce.orders.model.order.OrderCreateDTO;
 import br.com.ecommerce.orders.model.order.OrderStatus;
@@ -47,8 +54,8 @@ class OrderControllerUnitTest {
 
 
     @Test
-    @DisplayName("Create order - Should return a response containing the created order details")
-    void createTest() throws IOException, Exception {
+    @DisplayName("Unit - createOrder - Should return status 201 and created order details")
+    void createOrderTest01() throws IOException, Exception {
         // arrange
         var productsList = List.of(
             new ProductDTO(1L, 100),
@@ -61,26 +68,124 @@ class OrderControllerUnitTest {
             new Order(1L, productsList.stream().map(p -> new Product(p.id(), BigDecimal.ONE, p.unit())).toList());
         when(service.saveOrder(any(), any())).thenReturn(mockServiceReturn);
 
-        // act and assert
-        mvc.perform(post("/orders")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(orderCreateDTOJson.write(requestBody).getJson())
-            .header("X-auth-user-id", "1")
+        // act
+        mvc.perform(
+            post("/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderCreateDTOJson.write(requestBody).getJson())
+                .header("X-auth-user-id", "1")
+        )
+        // assert
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.products").isNotEmpty())
+        .andExpect(jsonPath("$.products", hasSize(3)))
+        .andExpect(jsonPath("$.totalOrder").value(300))
+        .andExpect(jsonPath("$.date").isNotEmpty())
+        .andExpect(jsonPath("$.status").value(OrderStatus.AWAITING_PAYMENT.toString()));
+
+        verify(service).saveOrder(any(), any());
+        verify(template).convertAndSend(anyString(), anyString(), any(PaymentDTO.class));
+        verify(template).convertAndSend(anyString(), anyString(), anyList());
+    }
+    @Test
+    @DisplayName("Unit - createOrder - Should return status 400 when the product list is empty or null")
+    void createOrderTest02() throws IOException, Exception {
+        // arrange
+        List<ProductDTO> productsList = List.of();
+        OrderCreateDTO requestBodyEmptyList = new OrderCreateDTO(productsList);
+        
+        // act
+        mvc.perform(
+            post("/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderCreateDTOJson.write(requestBodyEmptyList).getJson())
+                .header("X-auth-user-id", "1")
+        )
+        // assert
+        .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(service);
+        verifyNoInteractions(template);
+    }
+    @Test
+    @DisplayName("Unit - createOrder - Should return status 400 when X-auth-user-id header is missing")
+    void createOrderTest03() throws IOException, Exception {
+        // arrange
+        OrderCreateDTO validRequestBody = new OrderCreateDTO(List.of(
+            new ProductDTO(1L, 100),
+            new ProductDTO(2L, 100)
             )
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("$.products").isNotEmpty())
-            .andExpect(jsonPath("$.products", hasSize(3)))
-            .andExpect(jsonPath("$.totalOrder").value(300))
-            .andExpect(jsonPath("$.date").isNotEmpty())
-            .andExpect(jsonPath("$.status").value(OrderStatus.AWAITING_PAYMENT.toString()));
+        );
+        
+        // act
+        mvc.perform(
+            post("/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderCreateDTOJson.write(validRequestBody).getJson())
+                // missing X-auth-user-id header
+        )
+        // assert
+        .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(service);
+        verifyNoInteractions(template);
     }
 
     @Test
-    @DisplayName("Cancel order - Should return status 200")
-    void cancelOrderTest() throws IOException, Exception {
-        mvc.perform(patch("/orders/1")
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("X-auth-user-id", "1")
-        ).andExpect(status().isOk());
+    @DisplayName("Unit - getAllBasicsInfoOrdersByUser - Should return statud 400 when X-auth-user-id header is missing")
+    void getAllBasicsInfoOrdersByUserTest() throws Exception {
+        mvc.perform(
+            get("/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                // missing X-auth-user-id header
+        )
+        // assert
+        .andExpect(status().isBadRequest());
+        
+        verifyNoInteractions(service);
+    }
+    @Test
+    @DisplayName("Unit - getAllOrdersByUserId - Should return statud 400 when X-auth-user-id header is missing")
+    void getOrderByIdAndUserIdTest() throws Exception {
+        mvc.perform(
+            get("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                // missing X-auth-user-id header
+        )
+        // assert
+        .andExpect(status().isBadRequest());
+        
+        verifyNoInteractions(service);
+    }
+
+    @Test
+    @DisplayName("Unit - cancelOrder - Should return status 200")
+    void cancelOrderTest01() throws IOException, Exception {
+        // act
+        mvc.perform(
+            patch("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-auth-user-id", "1")
+        )
+        // assert
+        .andExpect(status().isNoContent());
+
+        verify(service).updateOrderStatus(anyLong(), any());
+        verify(template).convertAndSend(anyString(), anyString(), anyLong());
+    }
+    @Test
+    @DisplayName("Unit - cancelOrder - Should return status 400 when X-auth-user-id header is missing")
+    void cancelOrderTest02() throws IOException, Exception {
+        // act
+        mvc.perform(
+            patch("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                // missing X-auth-user-id header
+        )
+        // assert
+        .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(service);
+        verifyNoInteractions(template);
     }
 }
